9f4a2cfd8cda3a1a60fb89534f813cd712deaa5e
Michael Pawlowski <Michael.Pawlowski@accenture.com>
2020-01-15 10:19:57 -0500

    error handling and formatting

--- WordSearch.js
+++ WordSearch.js
@@ -1,14 +1,21 @@
 const response = require( './api.json' );
 
 class WordSearch {
-    constructor() {
-        this.puzzle = response.puzzle;
-        this.answers = response.answers;
-    }
+  constructor() {
+    this.puzzle = '';
+    this.answers = [];
 
-    get( which ) {
-        return this[ which ];
+    if( Math.sqrt( response.puzzle.length ) % 1 === 0 ) {
+      this.puzzle = response.puzzle;
+      this.answers = response.answers;
+    } else {
+      console.error( 'Puzzle is not a square. Exiting...' );
     }
+  }
+
+  get( which ) {
+    return this[ which ];
+  }
 }
 
 module.exports = WordSearch;
--- WordSearch.test.js
+++ WordSearch.test.js
@@ -1,13 +1,13 @@
 const WordSearch = require( './WordSearch' );
 
 test( 'WordSearch class exists', () => {
-    expect( new WordSearch() );
+  expect( new WordSearch() );
 } );
 
 test( 'API request gets puzzle and answers', () => {
-    const ws = new WordSearch();
+  const ws = new WordSearch();
 
-    expect( ws.get( 'puzzle' ).length ).toBeGreaterThan( 0 );
-    expect( typeof ws.get( 'answers' ) ).toBe( 'object' );
-    expect( ws.get( 'answers' ).length ).toBeGreaterThan( 0 );
+  expect( ws.get( 'puzzle' ).length ).toBeGreaterThan( 0 );
+  expect( typeof ws.get( 'answers' ) ).toBe( 'object' );
+  expect( ws.get( 'answers' ).length ).toBeGreaterThan( 0 );
 } );
