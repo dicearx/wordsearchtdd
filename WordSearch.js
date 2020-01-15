687e5df9944d45020c1a582dd148ac063463e434
Michael Pawlowski <Michael.Pawlowski@accenture.com>
2020-01-15 10:10:13 -0500

    mocking test data

--- WordSearch.js
+++ WordSearch.js
@@ -1,5 +1,14 @@
-class WordSearch {
+const response = require( './api.json' );
 
+class WordSearch {
+    constructor() {
+        this.puzzle = response.puzzle;
+        this.answers = response.answers;
+    }
+
+    get( which ) {
+        return this[ which ];
+    }
 }
 
 module.exports = WordSearch;
--- WordSearch.test.js
+++ WordSearch.test.js
@@ -1,5 +1,13 @@
 const WordSearch = require( './WordSearch' );
 
-test('WordSearch class exists', () => {
-    expect( WordSearch );
-});
+test( 'WordSearch class exists', () => {
+    expect( new WordSearch() );
+} );
+
+test( 'API request gets puzzle and answers', () => {
+    const ws = new WordSearch();
+
+    expect( ws.get( 'puzzle' ).length ).toBeGreaterThan( 0 );
+    expect( typeof ws.get( 'answers' ) ).toBe( 'object' );
+    expect( ws.get( 'answers' ).length ).toBeGreaterThan( 0 );
+} );
