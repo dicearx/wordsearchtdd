31bdb15867174f62cd45e2b0a8c66a30a50ae420
Michael Pawlowski <Michael.Pawlowski@accenture.com>
2020-01-16 11:58:54 -0500

    adding data transformation based on better understanding of incoming data

--- WordSearch.test.js
+++ WordSearch.test.js
@@ -8,6 +8,7 @@ test( 'API request gets puzzle and answers', () => {
   const ws = new WordSearch();
 
   expect( ws.get( 'puzzle' ).length ).toBeGreaterThan( 0 );
+
   expect( typeof ws.get( 'answers' ) ).toBe( 'object' );
   expect( ws.get( 'answers' ).length ).toBeGreaterThan( 0 );
 } );
