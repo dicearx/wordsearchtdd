a25b6ce8cb9ea19aedb4c9b0955f9f80a17cac5f
Michael Pawlowski <Michael.Pawlowski@accenture.com>
2020-01-17 11:25:13 -0500

    find a word horizaontally

--- WordSearch.js
+++ WordSearch.js
@@ -1,49 +1,17 @@
-// const response = require( './api.json' );
-// const response = `DEVELOPMENT,DRIVEN,KATA,TEST,THIS
-// D,P,E,T,K,H,S,Y,B,S,J,K,L,M,V
-// G,R,E,Q,Y,L,P,H,B,Z,N,J,W,Z,O
-// D,S,I,C,Q,S,S,M,L,O,K,U,S,R,S
-// T,J,V,V,D,H,Z,Z,O,J,W,G,F,G,W
-// Q,J,F,Z,E,O,Y,N,F,S,L,U,M,S,Z
-// P,A,Q,W,E,N,T,G,B,S,B,U,D,L,Y
-// Y,N,L,F,Y,P,F,W,C,S,A,C,Q,V,B
-// D,H,S,B,P,X,I,D,X,Q,M,T,W,E,R
-// V,P,E,J,T,A,P,G,Z,B,O,H,N,D,W
-// I,U,S,F,M,C,A,T,A,K,A,I,A,Z,X
-// U,G,W,U,S,X,X,A,I,K,P,S,N,F,D
-// Q,A,G,J,B,S,M,D,K,Q,V,W,M,A,F
-// Q,T,B,C,E,R,S,A,Z,S,G,M,O,F,E
-// Z,T,Q,G,U,L,O,B,B,X,O,Z,R,H,Y
-// D,E,V,E,L,O,P,M,E,N,T,O,Z,K,T`;
-const response = `BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA
-U,M,K,H,U,L,K,I,N,V,J,O,C,W,E
-L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G
-H,S,U,P,J,P,R,J,D,H,S,B,X,T,G
-B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E
-A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D
-S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F
-B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z
-O,K,R,I,K,A,M,M,R,M,F,B,A,P,P
-N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S
-E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K
-S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D
-T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K
-O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H
-W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S
-K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B`;
-
 class WordSearch {
-  constructor() {
+  constructor( res = response ) {
     this.puzzle = [];
     this.answers = [];
 
-    this.transformData( response );
+    this.transformData( res );
 
-    for( let a = 0; a < this.answers.length; a++ ) {
-    for( let i = 0; this.puzzle.length < i; i++ ) {
-      this.findAnswerHorizontally( this.puzzle[ i ], this.answers[ a ] );
-    }
-    }
+    // for( let a = 0; a < this.answers.length; a++ ) {
+    //   for( let i = 0; i < this.puzzle.length; i++ ) {
+    //     if( this.answerExistsHorizontally( this.answers[ a ], this.puzzle[ i ] ) ) {
+    //       console.log( `${this.answers[a]}: (${i})`);
+    //     }
+    //   }
+    // }
   }
 
   get( which ) {
@@ -51,14 +19,14 @@ class WordSearch {
   }
 
   transformData( res ) {
-    const lines = response.split( /\n/g );
+    const lines = res.split( /\n/g );
 
     if( lines.length > 1 ) {
       const answers = lines.shift();
 
       this.answers = answers.split( ',' );
 
-      this.puzzle = lines.map( ( line ) => line.split( /,/g ) );
+      this.puzzle = lines.map( ( line ) => line.replace( /,/g, '' ) );
 
       if( this.puzzle.length !== this.puzzle[ 0 ].length ) {
         console.error( 'Puzzle is not a square. Exiting...' );
@@ -70,10 +38,34 @@ class WordSearch {
     }
   }
 
-  findAnswerHorizontally( line, answer ) {
-    const answerRE = new RegExp( answer, ig );
+  buildAnswer( col, row, includeComma = true ) {
+    return `(${col},${row})${includeComma ? ',' : ''}`;
+  }
 
-    console.log( answer, line, answerRE.test( line.join( /,/ ) ) );
+  answerExistsHorizontally( answer, line ) {
+    const answerRE = new RegExp( answer, 'i' );
+
+    return line.match( answerRE );
+  }
+
+  findWordHorizontally( answer, lineIndex = null, positionIndex = null ) {
+    if( lineIndex !== null && positionIndex !== null ) {
+      let str = `${answer}: ${this.buildAnswer( positionIndex, lineIndex )}`;
+
+      for( let i = positionIndex + 1; i < answer.length; i++ ) {
+        str += `${this.buildAnswer( i, lineIndex, i < answer.length - 1 )}`;
+      }
+
+      return str;
+    } else {
+      for( let i = 0; i < this.puzzle.length; i++ ) {
+        const matches = this.answerExistsHorizontally( answer, this.puzzle[ i ] );
+
+        if( matches ) {
+          return this.findWordHorizontally( answer, i, matches.index );
+        }
+      }
+    }
   }
 }
 
--- WordSearch.test.js
+++ WordSearch.test.js
@@ -1,14 +1,47 @@
 const WordSearch = require( './WordSearch' );
 
-test( 'WordSearch class exists', () => {
-  expect( new WordSearch() );
-} );
+const ws1 = new WordSearch(`BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA
+U,M,K,H,U,L,K,I,N,V,J,O,C,W,E
+L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G
+H,S,U,P,J,P,R,J,D,H,S,B,X,T,G
+B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E
+A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D
+S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F
+B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z
+O,K,R,I,K,A,M,M,R,M,F,B,A,P,P
+N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S
+E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K
+S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D
+T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K
+O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H
+W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S
+K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B`);
+const ws2 = new WordSearch(`DEVELOPMENT,DRIVEN,KATA,TEST,THIS
+D,P,E,T,K,H,S,Y,B,S,J,K,L,M,V
+G,R,E,Q,Y,L,P,H,B,Z,N,J,W,Z,O
+D,S,I,C,Q,S,S,M,L,O,K,U,S,R,S
+T,J,V,V,D,H,Z,Z,O,J,W,G,F,G,W
+Q,J,F,Z,E,O,Y,N,F,S,L,U,M,S,Z
+P,A,Q,W,E,N,T,G,B,S,B,U,D,L,Y
+Y,N,L,F,Y,P,F,W,C,S,A,C,Q,V,B
+D,H,S,B,P,X,I,D,X,Q,M,T,W,E,R
+V,P,E,J,T,A,P,G,Z,B,O,H,N,D,W
+I,U,S,F,M,C,A,T,A,K,A,I,A,Z,X
+U,G,W,U,S,X,X,A,I,K,P,S,N,F,D
+Q,A,G,J,B,S,M,D,K,Q,V,W,M,A,F
+Q,T,B,C,E,R,S,A,Z,S,G,M,O,F,E
+Z,T,Q,G,U,L,O,B,B,X,O,Z,R,H,Y
+D,E,V,E,L,O,P,M,E,N,T,O,Z,K,T`);
 
 test( 'API request gets puzzle and answers', () => {
-  const ws = new WordSearch();
+  expect( ws1.get( 'puzzle' ).length ).toBeGreaterThan( 0 );
+  expect( ws1.get( 'answers' ).length ).toBeGreaterThan( 0 );
+} );
 
-  expect( ws.get( 'puzzle' ).length ).toBeGreaterThan( 0 );
+test( 'Should find SCOTTY horizontally', () => {
+  expect( ws1.findWordHorizontally( 'SCOTTY' ) ).toBe( 'SCOTTY: (0,5),(1,5),(2,5),(3,5),(4,5),(5,5)' );
+} );
 
-  expect( typeof ws.get( 'answers' ) ).toBe( 'object' );
-  expect( ws.get( 'answers' ).length ).toBeGreaterThan( 0 );
+test( 'Should find DEVELOPMENT horizontally', () => {
+  expect( ws2.findWordHorizontally( 'DEVELOPMENT' ) ).toBe( 'DEVELOPMENT: (0,14),(1,14),(2,14),(3,14),(4,14),(5,14),(6,14),(7,14),(8,14),(9,14),(10,14)' );
 } );
